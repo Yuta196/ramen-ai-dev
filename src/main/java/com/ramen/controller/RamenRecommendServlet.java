512d3bbb4df0e2acfb3af5e4fe595e2a7321fb0b
Yuta196 <yuta84q@gmail.com>
2025-07-20 01:39:39 +0900

    ■Mavenプロジェクトの設定ファイルを追加し、ラーメン店推薦機能を実装 ■ラーメン店エンティティとDAOを作成し、サンプルデータを追加 ■ラーメン推薦サーブレットを実装し、推薦ページを作成 ■web.xmlを更新し、サーブレットのマッピングを整理

--- src/main/java/com/ramen/controller/RamenRecommendServlet.java
+++ src/main/java/com/ramen/controller/RamenRecommendServlet.java
@@ -34,3 +34,26 @@ public class RamenRecommendServlet extends HttpServlet {
         }
     }
 }
+package com.ramen.controller;
+
+import com.ramen.entity.RamenShop;
+import com.ramen.model.RamenShopDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/recommend")
+public class RamenRecommendServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RamenShopDao dao = new RamenShopDao();
+        List<RamenShop> shopList = dao.findAll();
+        request.setAttribute("shopList", shopList);
+        request.getRequestDispatcher("/recommend.jsp").forward(request, response);
+    }
+}
